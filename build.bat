6b513fbefe3389b9c344479a260c8f3b1766e62d
Shaw <shawwian@gmail.com>
2023-09-23 19:40:00 -0400

    dont recompile orca runtime every compile

--- build.bat
+++ build.bat
@@ -17,8 +17,15 @@ set wasm_flags=--target=wasm32^
 
 if not exist build\ mkdir build
 pushd build
-clang %wasm_flags% -o .\module.wasm %ORCA_DIR%\src\orca.c %STDLIB_DIR%\src\*.c "%src_dir%\solitaire.c"
+
+if not exist liborca.a (
+   clang %wasm_flags% -Wl,--relocatable -o .\liborca.a %ORCA_DIR%\src\orca.c %STDLIB_DIR%\src\*.c
+   IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+)
+
+clang %wasm_flags% -o .\module.wasm  -L . -lorca "%src_dir%\solitaire.c"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
 orca bundle --orca-dir %ORCA_DIR% --name Solitaire --icon icon.png --resource-dir "%src_dir%\data" module.wasm
 popd
 
